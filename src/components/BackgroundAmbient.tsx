7c041ebd01f6e0c7b9a58cbe63006f0b672ce0c2
barto <18618583+rajlu@users.noreply.github.com>
2023-01-04 20:38:39 +0100

    feat: add static ambient background

---
+++ src/components/BackgroundAmbient.tsx
@@ -0,0 +1,12 @@
+import InlineSVG from 'react-inlinesvg';
+
+const BackgroundAmbient = () => {
+  return (
+    <InlineSVG
+      className="absolute -z-10 top-0 right-0 left-0 w-full h-auto"
+      src="/assets/background-ambient.svg"
+    />
+  );
+};
+
+export default BackgroundAmbient;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navigation from '@/components/Navigation';
+import BackgroundAmbient from '@/components/BackgroundAmbient';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <BackgroundAmbient />
       <Navigation />
       <Component {...pageProps} />
     </>
