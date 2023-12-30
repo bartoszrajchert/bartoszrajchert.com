f65844d7b5a1c5fdb856f4dc9085701674ba80fc
barto <18618583+rajlu@users.noreply.github.com>
2023-12-30 16:03:10 +0100

    fix: swap yarn to pnpm, conif next

--- components/BackgroundAmbient.tsx
+++ components/BackgroundAmbient.tsx
@@ -3,7 +3,7 @@ import InlineSVG from 'react-inlinesvg';
 const BackgroundAmbient = () => {
   return (
     <InlineSVG
-      className="absolute top-0 right-0 left-0 -z-10 h-auto w-full"
+      className="absolute left-0 right-0 top-0 -z-10 h-auto w-full"
       src="/assets/background-ambient.svg"
     />
   );
