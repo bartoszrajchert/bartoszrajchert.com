b6a38672da5e8a5e9eba243a3b48ee54804d0686
bartoszrajchert <18618583+bartoszrajchert@users.noreply.github.com>
2023-12-30 15:07:15 +0000

    Deploying to gh-pages from @ bartoszrajchert/bartoszrajchert.github.io@ad1b52108ce91e2528a3e999f39a20ea062057d8 🚀

---
+++ _next/static/XknEnEord5Y3qEiBd2d28/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-d2fe6114a325b0df.js"],"/_error":["static/chunks/pages/_error-95043b19f41f9a58.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
